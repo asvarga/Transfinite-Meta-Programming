8f3156605c28ea6c5e8c2ba542e8b71124da39b6
Alexander Varga <thealexvarga@gmail.com>
2023-02-11 22:17:03 -0500

    draw the main spine of OrdTrees

--- src/main/scala/OrdTree.scala
+++ src/main/scala/OrdTree.scala
@@ -19,22 +19,22 @@ sealed trait Node[T]:
   def map[S](f: T => S): Node[S]
 // part of the upward trie:
 case class Trie[T](l: TN[T] = OTNone[T](), r: TZN[T] = OTNone[T]()) extends Node[T]:
-  override def toString: String = s"(${l}, ${r})"
+  // override def toString: String = s"(${l}, ${r})"
   def map[S](f: T => S): Trie[S] = Trie(mapTN(l, f), mapTZN(r, f))
 // a zipper list (2 lists) of stacks of right nodes:
 case class Zipper[T](l: List[List[Node[T]]] = Nil, r: List[List[Node[T]]] = Nil) extends Node[T]:
-  override def toString: String = 
-    s"${l.map(L => L.mkString("[",",","]")).mkString("<<")}<>${r.map(L => L.mkString("[",",","]")).mkString(">>")}"
+  // override def toString: String = 
+    // s"${l.map(L => L.mkString("[",",","]")).mkString("<<")}<>${r.map(L => L.mkString("[",",","]")).mkString(">>")}"
   def map[S](f: T => S): Zipper[S] = Zipper(l.map(_.map(_.map(f))), r.map(_.map(_.map(f))))
 // a leaf of the tree / root of the zipper
 case class OrdTree[T](l: TN[T] = OTNone[T](), v: Option[T] = None) extends Node[T]:
   def move(so: SOrdinal): OrdTree[T] = move_(so, this)
   def set[S](t: T): OrdTree[T] = OrdTree(l, Some(t))
-  override def toString: String = s"${v}@${l}"
+  // override def toString: String = s"${v}@${l}"
   def map[S](f: T => S): OrdTree[S] = OrdTree(mapTN(l, f), v.map(f))
 // none
 case class OTNone[T]() extends Node[T]:
-  override def toString: String = "_"
+  // override def toString: String = "_"
   def map[S](f: T => S): OTNone[S] = OTNone()
 
 type TN[T] = Trie[T] | OTNone[T]
@@ -113,7 +113,7 @@ def testOrdTree() =
   // println(n.move(1).move(-1))
   // println(n.move(w))
   // println(n.move(w).move(-w))
-  val n  = OrdTree[Int]()
+  // val n  = OrdTree[Int]()
   // println(n)
   // println(n.set(5).v)
   // println(n.set(5).move(1).set(6).v)
@@ -124,9 +124,115 @@ def testOrdTree() =
   // println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).v)
   // println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).move(-w).move(-1).v)
   // println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).move(-w).move(-1).move(w).v)
-  println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).move(-w).move(-1).move(w).move(1))
-  println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).move(-w).move(-1).move(w).move(1).map(_+1))
+  // println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).move(-w).move(-1).move(w).move(1))
+  // println(n.set(5).move(1).set(6).move(w).set(7).move(-1).set(8).move(-w).move(-1).move(w).move(1).map(_+1))
   // println(n.set(5).move(1).set(6).move(w).set(7)
   //   .move(`ω^`(w)).move(-`ω^`(w))
   //   .move(-1).set(8).move(-w).move(-1).move(w).move(1).v)
 
+  val n2 = OrdTree[String]()
+  val ww = `ω^`(w)
+  val www = `ω^`(ww)
+  // draw(n2.set("A").move(w).set("B").move(1).set("C"))
+  // draw(n2.set("A").move(1).set("B").move(w).set("C"))
+  // draw(n2.set("A").move(w).set("B").move(w*w).set("C").move(w*w*w).set("D").move(ww).set("E"))
+  draw(n2.set("A").move(w).set("B").move(w*w).set("C").move(ww).set("E"))
+  // draw(n2.set("A").move(w).set("B").move(1).set("C").move(ww).set("E"))
+
+
+// neato out/*.dot -n -Tpng -O
+def draw(n: OrdTree[String]) =
+
+  case class Point(s: String = "", x: Int = 0, y: Int = 0, z: Int = 0)
+
+  import java.io._
+  val pw = new PrintWriter(new File("local/Z.dot" ))
+  def line(x: String) = 
+    pw.write(x + "\n")
+  line("digraph G {")
+  line("  splines=true;")
+  line("  node [shape=circle, style=filled, label=\"\"];")
+
+  var _id = 0
+  def id() = {_id += 1; _id}
+
+  def point(x: Int = 0, y: Int = 0, z: Int = 0, label: String = "", color: String = "white"): Point = 
+    val name = s"n_${id()}"
+    line(s"  $name [pos=\"${x*50-z*25},${y*50}!\", label=\"$label\", fillcolor=$color];")
+    // line(s"  $name [pos=\"${y*50},${z*50}!\", label=\"$label\", fillcolor=$color];")    // rotated for now
+    // line(s"  $name [pos=\"${-z*50},${y*50}!\", label=\"$label\", fillcolor=$color];")    // rotated for now
+    return Point(name, x, y, z)
+
+  def edge(x: Point, y: Point) = 
+    line(s"  ${x.s} -> ${y.s};")
+    x
+    
+
+  var _xmax = 0
+  def xmax() = {_xmax += 1; _xmax}
+  var _xmin = 0
+  def xmin() = {_xmin -= 1; _xmin}
+  var _ymin = 0
+  def ymin() = {_ymin -= 2; _ymin}
+
+
+    
+  def f(n: Node[String]): Option[Point] =
+    val hash = n.hashCode()
+    println(s"$hash\t<- $n")
+    val ret = n match
+      case OrdTree(l, v) => 
+        val label = v.getOrElse("")
+        val color = "orange"
+        f(l) match
+          case Some(pl) => Some(edge(point(0, pl.y-1, pl.z-1, label=label, color=color), pl))
+          case None => Some(point(0, ymin(), 0, label=label, color=color))
+      case OTNone() => None
+      case Zipper(l, r) => Some(point(0, ymin(), 0, color="pink"))
+      case Trie(l, r) => 
+        val color = "teal"
+        // go left first but priotritize right for placement
+        val pl = f(l)
+        f(r) match
+          case Some(pr) => 
+            val p = edge(point(0, pr.y-1, pr.z+1, color=color), pr)
+            pl match
+              case Some(pl) => Some(edge(p, pl))
+              case None => Some(p)
+          case None =>
+            pl match
+              case Some(pl) => Some(edge(point(0, pl.y-1, pl.z-1, color=color), pl))
+              case None => None
+        
+    println(s"$hash\t-> $ret")
+    ret
+    
+
+    // n.l match
+    // case OrdTree(OTNone(), v) => 
+    //   val x = xmax()
+    //   val y = ymax()
+    //   val name = node(x, y, 0, v.getOrElse(""), "red")
+      // edge(node(label=n.v.getOrElse("")), node(0, 1, 1, color="red"))
+    // val y = 0
+    // def findYs(n: OrdTree[String]): List[Int] = n match
+    //   case OrdTree(OTNone(), _) => Nil
+    //   case OrdTree(Trie(l, r), _) => findYs(l) ++ findYs(r)
+    // edge(point(label=n.v.getOrElse("")), point(0, 1, 1, color="red"))
+
+  // def 
+
+  f(n)
+  line("}")
+  pw.close
+  
+
+
+
+/*
+TODO:
+  - test
+  - use in interpreter as env
+  - write it up
+
+*/
